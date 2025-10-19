3e818b3dc1e7e9647cd7e5a6b4fb52834be26c57
meekMusa2 <90427566+meekMusa2@users.noreply.github.com>
2025-10-20 00:19:08 +0100

    Delete get-pubkey.js

--- get-pubkey.js
+++
@@ -1,7 +0,0 @@
-const { Keypair } = require('@solana/web3.js');
-require('dotenv').config();
-
-const secretKey = JSON.parse(process.env.PRIVATE_KEY);
-const keypair = Keypair.fromSecretKey(new Uint8Array(secretKey));
-
-console.log('Your Public Key:', keypair.publicKey.toString());
